c8df94837fc9b35adf7b755c06b84657333e233e
anniec9205 <danbi9205@gmail.com>
2021-05-02 21:39:51 +1000

    added all sections for generate markdown for recalling info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,37 @@
 function generateMarkdown(data, githubInfo) {
   return `# ${data.title}
 
-## description
+##description
 ${data.description}
 
+##table of contents
+- [description](#description)
+- [installation](#installation)
+- [usage](#usage)
+- [licence](#licence)
+- [contributing](#contributing)
+- [tests](#tests)
+- [questions](#questions)
 
+##installation
+${data.installation}
+
+##usage
+${data.usage}
+
+##licence
+${data.licence}
+
+##contributing
+${data.contributing}
+
+##tests
+${data.tests}
+
+##questions
+[Github Profile](${githubInfo.profile})
+
+[Email] (${githubInfo.email})
 
 `;
 }
