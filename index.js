fbbff8c195eacdfb75dddbcdef8054a260c09f29
anniec9205 <danbi9205@gmail.com>
2021-05-02 21:54:24 +1000

    added an array of questions

--- index.js
+++ index.js
@@ -7,7 +7,51 @@ const axios = require("axios");
 const generate = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{   type: "input",
+    name: "username",
+    message: "What is your Github user name?"
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+},
+{   type: "input",
+    name: "title",
+    message: "What is your project title?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "What is the description of your project"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "What is the installation instructions"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "What is the project usage"    
+},
+{
+    type: "input",
+    name: "licence",
+    message: "What is the project licence"
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "Who are the contributing parties" 
+},
+{
+    type: "input",
+    name: "tests",
+    message: "What are the the project tests"
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
